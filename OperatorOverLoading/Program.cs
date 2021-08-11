c4e7f73d16c482adeeeead60d88f9b3f675f922c
nazurudeen <nazurudeen@yahoo.com>
2021-08-11 20:00:01 +0530

    updated

---
+++ OperatorOverLoading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OperatorOverLoading
+{
+    public struct Complex
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex
+            {
+                Real = c1.Real + c2.Real,
+                Imaginary = c1.Imaginary + c2.Imaginary
+            };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Complex a = new Complex() { Real = 1, Imaginary = 2 };
+                Complex b = new Complex() { Real = 4, Imaginary = 8 };
+                Complex c = a + b;
+                Console.WriteLine(c.Imaginary);
+
+        }
+    }
+}
---
+++ Product/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProductOfSeriesNumber
+{
+    class Program
+    {
+        static public void LargestProductOfSeries(string input, int series)
+        {
+            double[] productArray = new double[input.Length - series + 1];
+            for (int i = 0; i < productArray.Length; i++)
+            {
+                productArray[i] = 1;
+                for (int j = i; j < series + i; j++)
+                {
+                    productArray[i] *= Char.GetNumericValue(input[j]);
+                }
+            }
+            var maxProduct = productArray[0];
+            for (int i = 0; i < productArray.Length; i++)
+            {
+                if (productArray[i] > maxProduct)
+                {
+                    maxProduct = productArray[i];
+                }
+            }
+            Console.WriteLine($"{maxProduct}");
+
+        }
+        static void Main(string[] args)
+        {
+            var input = "1027839564";
+            var series = 5;
+            LargestProductOfSeries(input, series);
+        }
+    }
+}
