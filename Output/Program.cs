eb7671c8f226a2b5874c856fd440c7786a095c68
nazurudeen <58662337+nazurudeen23@users.noreply.github.com>
2021-08-13 02:57:56 +0530

    Update Program.cs

--- Output/Program.cs
+++ Output/Program.cs
@@ -9,7 +9,7 @@ namespace Output
             string test1 = "Welcome";
             string test2 = "to Gislen Software";
             string test3 = test2;
-            Console.WriteLine((test3 == test2) + " " + (test2.Equals(test3)));
+            Console.WriteLine((test3 == test2) + " " + (test2.Equals(test3)));   //true    true
             Console.ReadLine();
         }
     }
